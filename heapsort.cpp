823cf93b4c02ee63a6debfd9ecd25304225263f4
vm2124 <vm21242124@gmail.com>
2023-06-20 18:49:11 +0530

    heap sort

---
+++ heapsort.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+// heapify is the process making heap from given array
+// we can find the no of leaf node by (size/2)+1 eg arr size of 5 (5/2)+1 3 there are 3 leaf nodes
+// ie they are already in correct position n/2+1 to n
+// we have to process only 1 to n/2+1 index
+
+void heapify(int arr[], int n, int i)
+{
+    int largest = i;
+    int left = 2 * i;
+    int right = 2 * i + 1;
+    if (left <= n && arr[largest] < arr[left])
+    {
+        largest = left;
+    }
+    if (right <= n && arr[largest] < arr[right])
+    {
+        largest = right;
+    }
+    if (largest != i)
+    {
+        swap(arr[largest], arr[i]);
+        heapify(arr, n, largest);
+    }
+}
+void heapsort(int arr[], int n)
+{
+    int size = n;
+    while (size >= 1)
+    {
+        swap(arr[size], arr[1]);
+        size--;
+        heapify(arr, size, 1);
+    }
+}
+int main()
+{
+
+    int arr[6] = {-1, 50, 54, 30, 40, 10};
+    int n = 5;
+    cout << "printing before heapify" << endl;
+    for (int i = 1; i <= n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    for (int i = n / 2; i > 0; i--)
+    {
+        heapify(arr, n, i);
+    }
+    cout << "printing after heapify" << endl;
+    for (int i = 1; i <= n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    heapsort(arr, n);
+    return 0;
+}
